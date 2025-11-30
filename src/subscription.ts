ba69800747e21948177c61d801feb6e7eddfb518
EsTrans <estransyz@163.com>
2025-11-30 15:47:45 +0800

    feat: 库街区更新弹窗

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,10 +4,10 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
+  id: 749,
   name: 'Subscription',
-  version: 0,
-  author: 'author',
+  version: 0.1,
+  author: 'EsTrans',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
